adfe8bdeef905e3898ec4011c4db8b9ec3b5c09c
Paulo Ribeiro <paulo@diffraction.pt>
2023-09-20 10:31:52 +0100

    Remove Statcounter tracking code

--- footer.php
+++ footer.php
@@ -15,23 +15,6 @@
 
 <?php wp_footer(); ?>
 
-<!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
-var sc_project=500520; 
-var sc_invisible=1; 
-var sc_security="56829007"; 
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
-</script>
-<noscript><div class="statcounter"><a title="Web Analytics
-Made Easy - StatCounter" href="http://statcounter.com/"
-target="_blank"><img class="statcounter"
-src="//c.statcounter.com/500520/0/56829007/1/" alt="Web
-Analytics Made Easy - StatCounter"></a></div></noscript>
-<!-- End of StatCounter Code for Default Guide -->
 
 <!-- <a href="https://diffraction.pt/misc/phpot/twoway">polemic</a> -->
 
